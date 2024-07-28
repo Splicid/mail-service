6dbe602388ddb0cbfa6e771061f4abf8bb25194a
Luis a <luis1abreu11@gmail.com>
2024-07-27 23:41:42 -0400

    remove key

--- vid-automation/main.py
+++ vid-automation/main.py
@@ -3,12 +3,12 @@ from ibm_watson import TextToSpeechV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
 def main():
-    authenticator = IAMAuthenticator('oxg622jI_ig3mTAyOBOURf9GP5HuPGSRM2mDTiYlhqUf')
+    authenticator = IAMAuthenticator('')
     text_to_speech = TextToSpeechV1(
         authenticator=authenticator
     )
 
-    text_to_speech.set_service_url('https://api.au-syd.text-to-speech.watson.cloud.ibm.com/instances/4e447fe0-e1f1-4767-9b90-cde953bc71e7')
+    text_to_speech.set_service_url('')
 
     voices = text_to_speech.list_voices().get_result()
     print(json.dumps(voices, indent=2))
